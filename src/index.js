136dd2f145f253cbfcc1d3270cbf17529e71a436
ajb96817 <ajb@andrewbrault.com>
2025-02-06 11:25:47 -1000

    remove unused service worker code

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 import App from './App';
 
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 root.render(React.createElement(App));
-
-serviceWorkerRegistration.register();
 
